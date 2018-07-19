04c7dd40ab15af84bd6cfea9e1fb4237afa0f046
Brent Adams <vikingbrent@gmail.com>
2018-07-19 04:12:20 -0400

    Todo list complete with data source and delegate methods

---
+++ Todooey/ToDoListViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  Todooey
+//
+//  Created by Brent Adams on 7/18/18.
+//  Copyright © 2018 Brent Adams. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mike", "Buy Eggos", "Destory Demogorgon"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+    }
+    
+    //MARK - Tableview Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        //Add a checkmark accessory when the row is selected or removes it when deselected
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        //Display an animatd highlight when the row is selected
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+       
+    }
+
+}
+
